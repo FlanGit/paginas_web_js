7c5f8750eeac680ac708f78efe12100e8eeefaf6
Francisco <pulin@hotmail.com>
2018-06-21 13:59:24 +0200

    hola mundo

---
+++ 01_Datos/Hola_Funcion.js
@@ -0,0 +1,14 @@
+/*
+    Javascript(@Oracle) --> ECMAScript --> ES6 / ES2015
+*/
+
+function saludar(user) {
+    var saludos
+    saludos='HOLA'
+    var complemento = 'FUNCION JS'
+    console.log(saludos,complemento,user)
+}
+
+saludar()
+saludar('pepe')
+saludar('Elena')
---
+++ 01_Datos/Hola_Mundo.js
@@ -0,0 +1,17 @@
+/* Declaracion de variable*/
+var saludos
+/* Inicializacion de Variables*/
+saludos="HOLA"
+/* Declaracion + inicializacion*/
+var user = "MUNDO"
+
+/*
+Tipos de datos
+- string
+- number
+- booleans (true / false)
+- object
+- undefined
+*/
+
+console.log(saludos,user)
