cae49bdf4a71a484b80d8d5408bd7eef7bb7a544
Francisco <pulin@hotmail.com>
2018-08-03 16:58:33 +0200

    05_funciones

--- 05_Funciones/funciones_1.js
+++ 05_Funciones/funciones_1.js
@@ -1,5 +1,5 @@
 function basica(x){
-
+ 
 }
 
 let y 
@@ -8,13 +8,15 @@ basica(y)
 
 // funciones multiparametros desconocidos
 // recoge en un array todos los parametros
+
 function miFuncion(...z){
-    console.log('Hola' + z[0])
-    console.log('Hola' + arguments[0])
+    console.log('Hola' + z[0]+ z[1]+ z[2])
+    console.log('Hola' + arguments[0]+ z[1]+ z[2])
 }
 
 let x = 21
 miFuncion(x)
 miFuncion(x,56)
 miFuncion(x,56,89)
+
 
