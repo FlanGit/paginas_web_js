0ede4f53ba232cd9b228d1618ade6452b6bcc0b1
Francisco <pulin@hotmail.com>
2018-08-30 20:12:45 +0200

    arrays

--- 05_Objetos/arrays_f.js
+++ 05_Objetos/arrays_f.js
@@ -14,6 +14,10 @@ aNombres.forEach(
     (item, i) => console.log(`El usuario ${i+1} se llama ${item}`)
 ) 
 
+console.log(`-----------1---------------`)
+
+
+
 let aNumbers = [2,5,7,8,9]
 aNumbers.forEach(
     (item) => {
@@ -22,6 +26,9 @@ aNumbers.forEach(
     }
 ) 
 
+console.log(`-----------2---------------`)
+
+
 let aCuadrados = aNumbers.map(
     (item) => {return item*item}
 )
@@ -29,18 +36,33 @@ let aCuadrados = aNumbers.map(
 // let aCuadrados = aNumbers.map(item => item*item)
 console.log(aCuadrados);
 
+console.log(`-----------3---------------`)
+
+// map elimina el ultimo elemento  "pila filo"
 let aNumbersClon = aNumbers.map(item => item)
 
 aNumbersClon.pop()
-console.log(aNumbersClon)
+console.log(aNumbersClon )
 console.log(aNumbers);
 
+console.log(`-----------4---------------`)
+
+
+// filter da como resultado un boolean y toma los verdaderos, true
+// aqui saca los items q son numeros pares
 let aPares = aNumbers.filter(
     (item) => {return !(item % 2)}
 )
 
+console.log(aPares);
+console.log(`-----------5---------------`)
+
+
 /* let aPares = aNumbers.filter(item => !(item % 2))
  */
+
+
+ 
 
 let aImpares = aNumbers.filter(
     (item) => {return !!(item % 2)}
@@ -48,19 +70,27 @@ let aImpares = aNumbers.filter(
 
 console.log(aPares)
 console.log(aImpares)
+console.log(`-----------6---------------`)
+
 
 
 let sonTodosPares = aNumbers.every(
     (item) => {return !(item % 2)}
 )
 console.log(sonTodosPares);
+console.log(`-----------7---------------`)
+
 
 let hayAlgunPar = aNumbers.some(
     (item) => {return !(item % 2)}
 )
 console.log(hayAlgunPar);
+console.log(`-----------8---------------`)
 
-let valorFinal = aNumbers.reduce((a,b) => a+b)
+let valorFinal = aNumbers.reduce((a,b) => a-b)
 console.log(valorFinal);
+console.log(`-----------9---------------`)
+
 valorFinal = aNumbers.reduceRight((a,b) => a+b)
 console.log(valorFinal);
+console.log(`-----------10---------------`)
